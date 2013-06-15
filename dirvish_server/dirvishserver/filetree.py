88fdbdb5b2953e0824caa725e248f0c8052751ed
Tobias Reese <tobias.reese@phimobile.com>
2013-06-16 01:43:20 +0200

    - fixed stuff

--- dirvish_server/dirvishserver/filetree.py
+++ dirvish_server/dirvishserver/filetree.py
@@ -19,15 +19,15 @@ class Filetree:
     def append(self, name, size):
         print name
         self.size = self.size + size
-        rname = name.replace(self.name, "", 1)
+        rname = name.replace(self.path, "", 1)
         rname = rname.lstrip("/")
         match = self.pattern.match(rname)
         if match:
             if not match.group(1) in self.childs:
-                if (self.name == '/'):
-                    self.childs[match.group(1)] = Filetree(match.group(1), self.name + match.group(1), 0)
+                if (self.path == '/'):
+                    self.childs[match.group(1)] = Filetree(match.group(1), self.path + match.group(1), 0)
                 else:
-                    self.childs[match.group(1)] = Filetree(match.group(1), self.name + "/" + match.group(1), 0)
+                    self.childs[match.group(1)] = Filetree(match.group(1), self.path + "/" + match.group(1), 0)
             self.childs[match.group(1)].append(name, size)
             print match.group(1)
 
