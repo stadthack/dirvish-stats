b97d73b8c9945e9d9fe7bd1e0d53fc5dd18cdb05
Tobias Reese <tobias.reese@phimobile.com>
2013-06-16 11:04:51 +0200

    - fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ echo "This file need environment Variables"
 
 echo "Copy Files to $TARGET_HOST:$TARGET_DIR"
 rsync --delete-delay -vrz --progress * $TARGET_HOST:$TARGET_DIR || exit 1
-ssh $TARGET_HOST "cd $TARGET_HOST && bash install.sh"
+ssh $TARGET_HOST "cd $TARGET_DIR && bash install.sh"
 
