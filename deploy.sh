cdf8081014e722fa37ee90c690edbd9cf7af112e
Tobias Reese <tobias.reese@phimobile.com>
2013-06-16 10:48:12 +0200

    - added js madness

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,15 @@
 
 echo "This file need environment Variables"
 
+export PATH=$PATH:/opt/node/bin
 
 echo "Copy Files"
 rsync --delete-delay -vrz --progress * $TARGET_DIR || exit 1
+
+cd dirvish-stats
+cd ui
+npm install
+rm -r ../../dirvish_server/dirvishserver/static/.h*
+rm -r ../../dirvish_server/dirvishserver/static/*
+node_modules/grunt-cli/bin/grunt deploy
+cd ../../dirvish_server
